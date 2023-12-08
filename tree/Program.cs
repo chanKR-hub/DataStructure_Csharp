fc2b318f24d4573b5b64452283fa828078f8931e
chankr <chan0822kr@gmail.com>
2023-12-08 16:36:39 +0900

    version : 1.1.2
    details : i just added basic Node.
    
    1. What I Learned :
    (there could be something wrong, if there is, Please Point it out.)
    
    - I needed to state Generic type
    when i want to use it
    ex )
     Line 8 : public Node<T> next;
     Line 16 : Node<string> ndString = new Node<string>("string");
    
    2. differences with Java :
    - No need to initialize the Next(attribute) to the null. C# initialize automatically the unsigned attribute that is type of class.

--- tree/Program.cs
+++ tree/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace tree
 {
     class Program
-    {
+    {  
+        class Node<T>  // Generi, Basic Node
+        {
+            public T Data; // Data
+            public Node<T> next; // Next Node
+            public Node(T data) // Node Innitialization
+            {
+                this.Data = data; 
+            }
+        }     
         static void Main(string[] args)
         {
-            
+            Node<string> ndString = new Node<string>("string");
+            Console.WriteLine(ndString.Data);
         }
     }
 }
